e015d18480dd9c337803ddd84a0d8dd21f334502
Robert Palmer <r91palmer@gmail.com>
2020-07-09 17:28:28 -0500

    added comments

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ container.addEventListener("mouseover", function(e){
 	}
 }); 
 
+// Set default size to 16
 createGrid(16);
 
 randomColor.addEventListener("click", function(e) {
 	container.addEventListener("mouseover", function(e){
+		// Callback function that fills different divs with random colors
 		if(e.target && e.target.matches("div.column")) {
 			e.target.classList.remove("isBlack");
 			e.target.style.background = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
@@ -30,6 +32,7 @@ reset.addEventListener("click", function(e){
 	chooseSize();
 });
 
+// function that creates grid size based on user input
 function chooseSize() {
 	let gridSize = prompt("Choose a number between 1 and 100");
 	console.log(gridSize);
@@ -43,6 +46,7 @@ function chooseSize() {
 	createGrid(gridSize);
 }
 
+// function to create the grid using nested for loops
 function createGrid(n) {
 	for(var row = 1; row <= n; row++) {
 		const divRow = document.createElement("div");
